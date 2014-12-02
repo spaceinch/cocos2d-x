c0173c8edaa59f38f083a0330ebe3b4e762c74dc
Matthew Miller <mmiller@Matthews-MacBook-Pro.local>
2014-12-02 12:35:23 -0800

    Android.mk: Added search path for local headers in extensions

--- extensions/Android.mk
+++ extensions/Android.mk
@@ -35,7 +35,8 @@ LOCAL_STATIC_LIBRARIES += box2d_static
 
 LOCAL_CXXFLAGS += -fexceptions
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)
+LOCAL_C_INCLUDES := $(LOCAL_PATH) \
+                    $(LOCAL_PATH)/../cocos
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/. \
                            $(LOCAL_PATH)/.. \
