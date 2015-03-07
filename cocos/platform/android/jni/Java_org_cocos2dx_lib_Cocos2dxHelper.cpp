9abf2e0f5a99cc2982a29db03280a2c138a01092
Matthew Miller <mmiller@Matthews-MacBook-Pro.local>
2015-03-06 16:09:02 -0800

    Added method to get external storage

--- cocos/platform/android/jni/Java_org_cocos2dx_lib_Cocos2dxHelper.cpp
+++ cocos/platform/android/jni/Java_org_cocos2dx_lib_Cocos2dxHelper.cpp
@@ -168,6 +168,20 @@ std::string getFileDirectoryJNI() {
     return ret;
 }
 
+std::string getExternalStorageDirectory() {
+    JniMethodInfo t;
+    std::string ret("");
+    
+    if (JniHelper::getStaticMethodInfo(t, CLASS_NAME, "getExternalStorageDirectory", "()Ljava/lang/String;")) {
+        jstring str = (jstring)t.env->CallStaticObjectMethod(t.classID, t.methodID);
+        t.env->DeleteLocalRef(t.classID);
+        ret = JniHelper::jstring2string(str);
+        t.env->DeleteLocalRef(str);
+    }
+    
+    return ret;
+}
+
 std::string getCurrentLanguageJNI() {
     JniMethodInfo t;
     std::string ret("");
