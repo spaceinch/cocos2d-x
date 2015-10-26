0b2c9937362b89863053d5c5dccb34b61903d6aa
Ignacio Coso <Ignacio.coso@plungeinteractive.com>
2015-10-26 19:50:17 +0100

    Avoid crash (violated assert) on undeveloped methods.

--- cocos/ui/UILayout.cpp
+++ cocos/ui/UILayout.cpp
@@ -462,7 +462,7 @@ void Layout::scissorClippingVisit(Renderer *renderer, const Mat4& parentTransfor
 void Layout::setClippingEnabled(bool able)
 {
 #ifdef DIRECTX_ENABLED
-	CCASSERT(false, "Layout::setClippingEnabled is not supported");
+	//CCASSERT(false, "Layout::setClippingEnabled is not supported");
 #else
     if (able == _clippingEnabled)
     {
